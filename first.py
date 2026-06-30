79da03bd95eb8a4c68984949f6b7c2b54d592d6f
yasmithasai13-afk <yasmithasai13@gmail.com>
2026-06-30 06:49:51 +0530

    Update first.py

--- first.py
+++ first.py
@@ -2,9 +2,18 @@ import random
 class NumberGuess:
     def __init__(self):
         self.secret_number = None
-        self.range_limit = int(input("enter the range:"))
+        self.difficulty_lvl = int(input("enter the difficulty level(easy-1,medium-2,hard-3):"))
     def generate_secret_num(self):
-        self.secret_number = random.randint(0, self.range_limit)
+        for i in range(3):
+            print("generating secret number...")
+        if self.difficulty_lvl==1:
+            self.secret_number = random.randint(0,10)
+        elif self.difficulty_lvl==2:
+            self.secret_number=random.randint(0,30)
+        elif self.difficulty_lvl==3:
+            self.secret_number=random.randint(0,150)
+        else:
+            print("invalid input 🚫")
     def get_user_guess(self):
         self.user_guess = int(input("enter your guess:"))
     def check_guess(self):
